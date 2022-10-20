673a33db1e6aa87460ab87e357d40ad4574606e8
Taner Özer <tanerozer16@gmail.com>
2022-10-20 15:50:37 +0300

    data edited

---
+++ LegendTeam/mylegendteam/lib/data/data.dart
@@ -0,0 +1,95 @@
+class Strings {
+  static const String APP_NAME = "Legend-Team";
+  static const List<String> Footballer_Name = [
+    "Tafarel",
+    "Cafu",
+    "Sergio Ramos",
+    "Alessandro Nesta",
+    "Van Bronckhorst",
+    "Guti",
+    "De Bruyne",
+    "Iniesta",
+    "Fenomono Ronaldo",
+    "Messi",
+    "Hagi",
+  ];
+  static const List<String> Footballer_IdolPlayer = [
+    "Pele",
+    "Pele",
+    "Balondorma",
+    "Semih",
+    "Xavi",
+    "Guti",
+    "Maradona",
+    "Aleks",
+    "Arda",
+    "Zidane",
+    "Pele"
+  ];
+  static const List<String> Footballer_Age = [
+    "1966",
+    "1970",
+    "1986",
+    "1976",
+    "1975",
+    "1976",
+    "1991",
+    "1984",
+    "1976",
+    "1987",
+    "1965",
+  ];
+
+  static const List<String> Footballer_Detail = [
+    "Cláudio André Mergen Taffarel, eski futbolcu ve futbol antrenörüdür. Kaleci pozisyonunda görev aldığı futbolculuk kariyerini sonlandırmış olup, Galatasaray ve Brezilya millî takımında kaleci antrenörü olarak görev yapmıştır.",
+    "Cafu, Brezilyalı sağ bek oyuncusu. Brezilya millî futbol takımının formasını en fazla giyen futbolcu olan Cafu, futbola 1988'de São Paulo'da başladı. São Paulo'daki 5 sezonunda 3 Libertadores Kupası kazanan Cafu, 1995'te Real Zaragoza'ya transfer oldu",
+    "Sergio Ramos García stoper mevkiinde oynayan Ligue 1 ekiplerinden Paris Saint-Germain takımında forma giyen İspanyol futbolcudur. 2019'da The Guardian'ın 'En İyi 100 Erkek Futbolcu' listesinde en iyi dördüncü stoper olarak yer aldı.",
+    "Alessandro Nesta, İtalyan eski futbolcudur. Şu an Miami FC takımını çalıştırmaktadır. Milan'ın formasını giyen Alessandro Nesta futbola Lazio'da başladı. 2002 Yılında Silvio Berlusconi tarafından 35.5 Milyon Euro bedelle AC Milan'a transfer edildi.",
+    "Giovanni van Bronckhorst Endonezya asıllı Hollandalı teknik direktör, eski millî futbolcudur.",
+    "Jose Maria Gutiérrez Hernández veya kısaca Guti, orta saha mevkiinde görev yapmış İspanyol eski millî futbolcudur. Real Madrid'de oynadığı dönemde takımın 2. kaptanı olmuştur. Kulüp tarihinin en fazla formasını giyen 10 futbolcu arasındadır ve 2008 yılında takımının ligde 5000. golünü atmıştır.",
+    "Kevin De Bruyne, ofansif orta saha ve kanat mevkilerinde görev alabilen Belçikalı millî futbolcudur. Premier League ekiplerinden Manchester City'de forma giymektedir.",
+    "Andrés Iniesta Luján, İspanyol millî futbolcudur. Şu an Vissel Kobe'de forma giymektedir. İspanya adına mücadele etmiştir. Orta sahanın ofansif bölgesinde oynamaktadır.",
+    "Ronaldo Luís Nazário de Lima, genellikle Ronaldo olarak bilinen, Brezilyalı bir işletme sahibi, La Liga kulübü Valladolid'in başkanı, Brezilya Série B kulübü Cruzeiro'nun sahibi ve forvet olarak oynayan Brezilyalı emekli bir futbolcudur",
+    "Lionel Andrés Messi Cuccittini, ayrıca Leo Messi olarak da bilinir, Ligue 1 takımlarından Paris Saint-Germain'de forma giyen ve Arjantin millî takımının kaptanlığını yapan Arjantinli futbolcudur.",
+    "Gheorghe Hagi, orta saha mevkiisinde oynamış Rumen eski futbolcu, teknik direktördür. Karpatların Maradonası lakabı ile anılsa da bundan hiç hoşlanmamıştır. ",
+  ];
+  static const List<String> Footballer_Img = [
+    'assets/images/tafarel.png',
+    'assets/images/cafu.jpg',
+    'assets/images/ramos.jpg',
+    'assets/images/nesta.jpg',
+    "assets/images/van.png",
+    "assets/images/guti.jpg",
+    "assets/images/debruyne.jpg",
+    "assets/images/iniesta.png",
+    "assets/images/ronaldo.jpg",
+    "assets/images/messi.png",
+    "assets/images/hagi.jpg",
+  ];
+  static const List<String> Footballer_Video = [
+    "UscyoZc9nj4?t=16",
+    "-PjnJvP6Nn0",
+    "ZL54xup_WLw",
+    "3WGXJd41aHI",
+    "uvmx_EC42oE",
+    "YerURodi4tM",
+    "AVfZHc_WXvs",
+    "RIbmWdYEV18",
+    "phtQaPqhIjA",
+    "F-nxbw-O9VY",
+    "OeK03Av0n4g",
+  ];
+  static const List<List<String>> Footballer_PlayerFav = [
+    ["Fatih Terim", "Hagi", "Okan"],
+    ["Maradona", "Ronaldinho"],
+    ["Maradona", "Puyol", "Nesta", "Delgado"],
+    ["Delgado", "Alex", "Burdisso"],
+    ["Hamit Altıntop", "Messi"],
+    ["Semih Kaya", " KralMessi", "sad"],
+    ["Fatih", "Semih Kaya", "Zico"],
+    ["Terim", "Ali Koç", "Messi"],
+    ["Aziz", "Cocu"],
+    ["Ronaldo", "Hagi", "Harun"],
+    ["Pele", "Arda Turan"],
+  ];
+}
