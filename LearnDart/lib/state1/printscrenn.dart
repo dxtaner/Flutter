ef42681ceff525a3488e122fcc5918c2933a1d14
Taner Özer <tanerozer16@gmail.com>
2022-10-21 14:45:17 +0300

    print

---
+++ LearnDart/lib/state1/printscrenn.dart
@@ -0,0 +1,17 @@
+void main(List<String> args) {
+  print("I learn Dart & Flutter");
+  print("2 + 5");
+  print('9 - 9');
+
+  print(9 * 9);
+  print(9 - 9);
+  // print("9/9=" + 9:9);
+
+  print("taner");
+  print("object");
+
+  /* comment line
+  print("9-0" + (9 - 0));
+  print("9-0" + (9 - 0));
+  */
+}
