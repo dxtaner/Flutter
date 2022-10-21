7acaa285d324ddfa9ddbd97a66f214aa9d7d7dfd
Taner Özer <tanerozer16@gmail.com>
2022-10-21 14:44:03 +0300

    datadypes

---
+++ LearnDart/lib/state1/numbers_boolean_datatypes.dart
@@ -0,0 +1,45 @@
+void main(List<String> args) {
+  int age = 25;
+  print(age);
+  print("yas");
+
+  age = 25;
+  print(age);
+
+  age = 25.4.toInt(); // toInt int cevrimi
+  print(age);
+
+  double kilo = 69.6;
+  print(kilo);
+
+  kilo = 69;
+  print(kilo);
+
+  num sicaklik = 20;
+  sicaklik = 21.3;
+  print(sicaklik);
+
+  int sayi1;
+  // print(sayi1);
+
+  int hexadecimalSayi = 0xDACCEF;
+  print(hexadecimalSayi);
+
+  double exponentsSayi = 1.22e6;
+  print(exponentsSayi);
+
+  var sayi2 = 300;
+  print(sayi2);
+  sayi2 = 30;
+  // sayi2 = 10.65; // hata
+
+  var ondalikSayi = 50.60;
+  ondalikSayi = 50.56;
+  print(ondalikSayi);
+
+  bool isNull = false;
+  print(isNull);
+
+  var isFalse = true;
+  print(isFalse);
+}
