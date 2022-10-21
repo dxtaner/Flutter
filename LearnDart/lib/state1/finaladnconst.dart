181dd75c58cd24a6eebc797279212a1579b684d2
Taner Özer <tanerozer16@gmail.com>
2022-10-21 14:46:30 +0300

    final const

---
+++ LearnDart/lib/state1/finaladnconst.dart
@@ -0,0 +1,15 @@
+main(List<String> args) {
+  final String sehirAdi = "Bursa";
+  // sehirAdi = "Istanbul";
+
+  const PI = 3.14;
+  //PI = 6.5;
+
+  print(sehirAdi);
+  print(PI);
+}
+/*
+class Ogrenci{
+  final ogrenciAdi ="taner";
+  static const numara = 52;
+}*/
