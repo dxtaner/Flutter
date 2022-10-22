69b1064ad4011d21f30170d825a4d280c902caae
Taner Özer <tanerozer16@gmail.com>
2022-10-22 15:15:34 +0300

    nuulable

---
+++ LearnDart/lib/Nullable/nullable.dart
@@ -0,0 +1,9 @@
+void main(List<String> args) {
+  //nullable
+  int? a;
+  a = null;
+  print('a değişkeninin değeri $a');
+
+  //a null bir değer olduğu için başka bir sayıyla toplayamıyor print(a + 2);
+  // print(a + 20);
+}
---
+++ LearnDart/lib/Nullable/nullable2.dart
@@ -0,0 +1,9 @@
+void main(List<String> args) {
+  List<String> stringListesi = ["taner", "ozer", "messi"];
+  List<String>? nullOlabilecekStringListesi;
+  List<String?> nullOlabilecekleriTutanListe = ["taner", null, "messi"];
+
+  print("string listesi $stringListesi");
+  print("null olabilecek liste $nullOlabilecekStringListesi ");
+  print("nullOlabilecekStringleriTutanListe $nullOlabilecekleriTutanListe");
+}
